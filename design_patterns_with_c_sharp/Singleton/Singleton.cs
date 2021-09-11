430bc52b92fb2fb75ef2436988003e8476cf6662
jorge_edson <jorge.edson.bsi@gmail.com>
2021-09-11 02:52:07 -0300

    Singleton creation pattern implementation

---
+++ design_patterns_with_c_sharp/Singleton/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;        
+
+        public static Singleton GetInstance
+        {
+            get 
+            {
+                if (instance == null) 
+                {
+                    instance = new Singleton();
+                    Console.WriteLine("New Object Singleton Started");                
+                }
+
+                return instance;
+            }
+        }
+
+        public static void DisposeInstance()
+        {
+            instance = null;
+            Console.WriteLine("The instance was destroyed");
+        }
+
+        public void isInstanceWhithMe(string nameObject) 
+        {
+            if (instance != null)
+            {
+                Console.WriteLine("The instance at this moment belongs to the object: " + nameObject);
+            }
+            else
+            {
+                Console.WriteLine(nameObject+" says: There is no instance");
+            }            
+        }        
+    }
+}
